6207ab71118685e88b9b33af434c6b9d65028fbc
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-12-21 16:39:12 +0900

    Add: lesson 4.5.2

--- hakenmoto/hakenshain.rb
+++ hakenmoto/hakenshain.rb
@@ -3,6 +3,4 @@ class Hakenshain
   def kiritsu
     puts '派遣社員は元気に立ち上がりました。'
   end
-
-  alias standup kiritsu
 end
---
+++ hakenmoto/hakenyobidashi.rb
@@ -0,0 +1,6 @@
+require_relative '../hakenmoto/hakenshain'
+
+# Hakenshain class for lesson 4.5.2
+class Hakenshain
+  alias standup kiritsu
+end
