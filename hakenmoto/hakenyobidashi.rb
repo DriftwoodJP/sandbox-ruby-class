31f110d82a07acb3301ac12828c2c0ffc1a36d3e
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-12-21 17:25:12 +0900

    Add: lesson 4.5.3

--- hakenmoto/hakenyobidashi.rb
+++
@@ -1,6 +0,0 @@
-require_relative '../hakenmoto/hakenshain'
-
-# Hakenshain class for lesson 4.5.2
-class Hakenshain
-  alias standup kiritsu
-end
---
+++ ukeireshain.rb
@@ -0,0 +1,8 @@
+require_relative './hakenmoto/hakenshain'
+
+# Ukeireshain class for lesson 4.5.3
+class Ukeireshain < Hakenshain
+  def standup
+    kiritsu
+  end
+end
