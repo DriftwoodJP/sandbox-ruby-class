f8a791021ffa24bf3c43638349c1d59e6da5a95e
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-12-22 15:10:11 +0900

    Clean: project files

--- shacho.rb
+++
@@ -1,9 +0,0 @@
-require_relative './shain_factory'
-
-shain_factory = ShainFactory.new
-
-%w[Tanto Shunin Bucho Torishimariyaku].each do |yakushoku|
-  shain = shain_factory.create(yakushoku, 100)
-  puts shain.standup
-  puts "私の給料は#{shain.calculate_salary}円です。"
-end
